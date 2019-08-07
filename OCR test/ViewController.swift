77bd5bfe38bf9b5732667dc5b4d5d9ad208338b0
Maciej Madaj <maciej.madaj@schibsted.pl>
2019-08-07 21:59:00 +0200

    Initial Commit

---
+++ OCR test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR test
+//
+//  Created by Maciej Madaj on 07/08/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
